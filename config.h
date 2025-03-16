bf99527e467951e1403477b60261c4ab95ada2b2
Simon Berger <simon.berger@inomotech.com>
2025-03-16 00:53:15 +0000

    bump version

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG
 #define CONFIG
 
-#define VERSION             301
+#define VERSION             302
 
 //try to keep this in sync.
 //SavvyCAN will complain if you connect a GVRET board with a revision
